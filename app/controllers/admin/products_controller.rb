a49cd80e1549f0f9679547244bdff381652903a4
ThomasDebek <tom.debek@gmail.com>
2023-03-13 20:03:34 +0100

    fix: add bullet gem and modification our admin views

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,10 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @products = Product.all
+    #@products = Product.includes([:category]).includes([:brand]).all
+
+    @products = Product.includes([:main_image_attachment]).all
+    @products = @products.filter_by_category(params[:category]) if params[:category].present?
+    @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
   end
 end
