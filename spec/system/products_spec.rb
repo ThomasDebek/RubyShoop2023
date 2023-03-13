330124be6b96959d75993a18092bc1979e1c2242
ThomasDebek <tom.debek@gmail.com>
2023-03-14 00:49:06 +0100

    test: add product request and system speces

---
+++ spec/system/products_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Products", type: :system do
+  context 'when creating a new one' do
+    before do
+      login_as(create(:admin))
+      visit new_admin_product_url
+    end
+
+    it 'fails with missing name' do
+      fill_in 'product_price', with: '100'
+      click_button 'Create Product'
+
+      expect(page).to have_content 'Name can\'t be blank'
+    end
+
+    it 'fails with missing price' do
+      fill_in 'product_name', with: 'test_product'
+      click_button 'Create Product'
+
+      expect(page).to have_content 'Price can\'t be blank'
+    end
+  end
+end
