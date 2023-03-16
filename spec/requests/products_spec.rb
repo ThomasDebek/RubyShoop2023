1777e8993f05f877de48cae30430a0d243c222f3
ThomasDebek <tom.debek@gmail.com>
2023-03-16 23:45:01 +0100

    test: refactor product tests

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,33 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "POST /admin/products", type: :request do
+RSpec.describe "Products", type: :request do
   before do
     login_as(create(:admin))
   end
 
-  context 'when attributes are valid' do
-    it 'creates new product' do
-      post '/admin/products', params: {
-        product: { name: 'new_product', price: '100'}
-      }
-      expect(Product.count).to eq(1)
-    end
-  end
+  describe 'POST /admin/products' do
+    let(:product) { create(:product) }
 
-  context 'when attributes are invalid' do
-    it 'does not create a new product without a name' do
-      post '/admin/products', params: {
-        product: { price: '100'}
-      }
-      expect(Product.count).to eq(0)
+
+    context 'when attributes are valid' do
+      it 'creates new product' do
+        post '/admin/products', params: {
+          product: { name: 'new_product', price: '100'}
+        }
+        expect(Product.count).to eq(1)
+      end
     end
 
-    it 'does not create a new product without a price' do
-      post '/admin/products', params: {
-        product: { name: 'new_product'}
-      }
-      expect(Product.count).to eq(0)
+    context 'when attributes are invalid' do
+      it 'does not create a new product without a name' do
+        post '/admin/products', params: {
+          product: { price: '100'}
+        }
+        expect(Product.count).to eq(0)
+      end
+
+      it 'does not create a new product without a price' do
+        post '/admin/products', params: {
+          product: { name: 'new_product'}
+        }
+        expect(Product.count).to eq(0)
+      end
     end
+
   end
 
 
