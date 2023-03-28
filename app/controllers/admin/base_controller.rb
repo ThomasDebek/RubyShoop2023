4cd4b32a39714c27d12ada710eab2c2db9efddcb
ThomasDebek <tom.debek@gmail.com>
2023-03-28 11:08:52 +0200

    fixed: modules after used rubocop

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-module Admin
-  class BaseController < ApplicationController
-    layout 'admin'
-    before_action :authenticate_admin!
-    before_action :admin_authentication
+class Admin::BaseController < ApplicationController
+  layout 'admin'
+  before_action :authenticate_admin!
+  before_action :admin_authentication
 
-    def admin_authentication
-      return if admin_signed_in?
-
+  def admin_authentication
+    unless admin_signed_in?
       flash[:alert] = 'You are not authorized.'
       redirect_to root_path
     end
   end
+
 end
+
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,43 +1,42 @@
 # frozen_string_literal: true
 
-module Admin
-  class ProductsController < Admin::BaseController
-    # layout 'admin'
-    # before_action :authenticate_admin!
+class Admin::ProductsController < Admin::BaseController
+  # layout 'admin'
+  # before_action :authenticate_admin!
 
-    def index
-      # @products = Product.includes([:category]).includes([:brand]).all
+  def index
+    # @products = Product.includes([:category]).includes([:brand]).all
 
-      @products = Product.includes([:main_image_attachment]).all
-      @products = @products.filter_by_category(params[:category]) if params[:category].present?
-      @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
-    end
+    @products = Product.includes([:main_image_attachment]).all
+    @products = @products.filter_by_category(params[:category]) if params[:category].present?
+    @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
+  end
 
-    def new
-      @product = Product.new
-    end
+  def new
+    @product = Product.new
+  end
 
-    def create
-      @product = Product.new(product_params)
+  def create
+    @product = Product.new(product_params)
 
-      if @product.save
-        redirect_to admin_products_path, notice: 'Product added successfully'
-      else
-        render :new
-      end
-    end
-
-    def destroy
-      @product = Product.find(params[:id])
-      @product.destroy
-
-      redirect_to admin_products_path, notice: 'Product deleted successfully'
-    end
-
-    private
-
-    def product_params
-      params.require(:product).permit(:name, :price, :category_id, :brand_id, :main_image)
+    if @product.save
+      redirect_to admin_products_path, notice: 'Product added successfully'
+    else
+      render :new
     end
   end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to admin_products_path, notice: 'Product deleted successfully'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id, :brand_id, :main_image)
+  end
 end
+
