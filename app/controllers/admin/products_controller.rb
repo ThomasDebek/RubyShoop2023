cf20780886cf91eae06aa094c6839f40dd3e456c
ThomasDebek <tom.debek@gmail.com>
2023-03-28 12:47:43 +0200

    refactor: fix products eager laoging style

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,11 +5,7 @@ class Admin::ProductsController < Admin::BaseController
   # before_action :authenticate_admin!
 
   def index
-    # @products = Product.includes([:category]).includes([:brand]).all
-
-    @products = Product.includes([:main_image_attachment]).all
-    @products = @products.filter_by_category(params[:category]) if params[:category].present?
-    @products = @products.filter_by_brand(params[:brand]) if params[:brand].present?
+    @products = Product.includes(:category, :brand).all
   end
 
   def new
