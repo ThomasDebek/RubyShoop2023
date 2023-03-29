c1252414079834c5550c5e6c305867ae6f788b26
ThomasDebek <tom.debek@gmail.com>
2023-03-29 17:33:29 +0200

    refactor: fix style

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   # get 'carts/:id', to: 'carts#show', as: 'cart'
   #delete 'carts/:id', to: 'carts#destroy'
 
-  post 'carts', to: 'carts#add', as: 'add_to_cart'
+  post 'cart', to: 'carts#add', as: 'add_to_cart'
 end
